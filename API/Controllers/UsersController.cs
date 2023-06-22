be3ee0dce74e75abfec6c624b1daded7712b31ee
mihaiches <kes.sorin@gmail.com>
2023-06-22 07:32:51 +0300

    fixing registration

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -3,34 +3,37 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Authorization;
 using API.Data;
 using API.Entities;
+using API.Interfaces;
+using AutoMapper;
+using API.DTOs;
 
 namespace API.Controllers
 {
     [Authorize]
     public class UsersController : BaseApiController
     {
-        private readonly DataContext _context;
+        private readonly IUserRepository _userRepository;
+        private readonly IMapper _mapper;
 
-        public UsersController(DataContext context)
+        public UsersController(IUserRepository userRepository, IMapper mapper)
         {
-            _context = context;
+            _mapper = mapper;
+            _userRepository = userRepository;
+            
         }     
 
-        [AllowAnonymous]
         [HttpGet]
-        public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
+        public async Task<ActionResult<IEnumerable<MemberDto>>> GetUsers()
         {
-            var users = await _context.Users.ToListAsync();
+            var users = await _userRepository.GetMembersAsync();
 
-            return users;
+            return Ok(users);
         }
 
-        [HttpGet("{id}")]
-        public async Task <ActionResult<AppUser>> GetUser(int id)
+        [HttpGet("{username}")]
+        public async Task <ActionResult<MemberDto>> GetUser(string username)
         {
-            var user = await _context.Users.FindAsync(id);
-
-            return user;
+            return await _userRepository.GetMemberAsync(username);
         }
     }
 }
